58e4d9aa18ad1a75077541cf21a5938bb35ee61a
giulia.nardi <giulia01.nardi@studenti.unicam.it>
2026-07-12 18:26:30 +0200

    aggiunto JavaFX, eliminato Swing

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130675/controller/GeneratorePianoDiStudi.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.mpgc.rpg130675.controller;
+
+public interface GeneratorePianoDiStudi {
+}
---
+++ src/main/java/it/unicam/cs/mpgc/rpg130675/controller/PianoInformaticaComunicazione.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.mpgc.rpg130675.controller;
+
+import it.unicam.cs.mpgc.rpg130675.model.esame.Esame;
+
+import java.util.List;
+
+public class PianoInformatica implements GeneratorePianoDiStudi{
+    @Override
+    public List<Esame> generaEsami() {
+        return List.of();
+    }
+}
